db7912da9ab20503171f11a1ac640175996520e5
ambareesh1 <ambru333@gmail.com>
2022-07-29 09:47:11 +0530

    1 file modified

--- src/manager/permissions/superadmin.permission.ts
+++ src/manager/permissions/superadmin.permission.ts
@@ -2,8 +2,30 @@ import { PermissionType } from '../permission-type';
 import { PermissionBase } from './base.permissions';
 
 export class AdminPermission extends PermissionBase {
-  constructor() {
-    super();
+  constructor(role: Role, page: string) {
+    super(role, page);
+
+    switch (page) {
+      case 'HD':
+      case 'DataFlow':
+      case 'OrganizationManagement':
+      case 'CityManagement':
+        this.permissions = [
+          PermissionType.CREATE,
+          PermissionType.READ,
+          PermissionType.UPDATE,
+          PermissionType.ACCESS,
+        ];
+        break;
+      case 'FacilityManagement':
+        this.permissions = [
+          PermissionType.CREATE,
+          PermissionType.READ,
+          PermissionType.UPDATE,
+          PermissionType.ACCESS,
+        ];
+    }
+
     this.permissions = [
       PermissionType.CREATE,
       PermissionType.READ,
